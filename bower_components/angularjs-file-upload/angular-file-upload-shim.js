cf36028729f977ade392535364b2df74f3ff5a41
Greg Michalec <greg@primate.net>
2014-08-08 18:41:59 -0400

    update libraries

--- bower_components/angularjs-file-upload/angular-file-upload-shim.js
+++ bower_components/angularjs-file-upload/angular-file-upload-shim.js
@@ -1,7 +1,7 @@
 /**!
  * AngularJS file upload shim for HTML5 FormData
  * @author  Danial  <danial.farid@gmail.com>
- * @version 1.3.1
+ * @version 1.6.4
  */
 (function() {
 
@@ -14,159 +14,187 @@ var hasFlash = function() {
 	}
 	return false;
 }
-	
+
+var patchXHR = function(fnName, newFn) {
+	window.XMLHttpRequest.prototype[fnName] = newFn(window.XMLHttpRequest.prototype[fnName]);
+};
+
 if (window.XMLHttpRequest) {
-	if (window.FormData) {
+	if (window.FormData && (!window.FileAPI || !FileAPI.forceLoad)) {
 		// allow access to Angular XHR private field: https://github.com/angular/angular.js/issues/1934
-		window.XMLHttpRequest = (function(origXHR) {
-			return function() {
-				var xhr = new origXHR();
-				xhr.setRequestHeader = (function(orig) {
-					return function(header, value) {
-						if (header === '__setXHR_') {
-							var val = value(xhr);
-							// fix for angular < 1.2.0
-							if (val instanceof Function) {
-								val(xhr);
-							}
-						} else {
-							orig.apply(xhr, arguments);
-						}
+		patchXHR("setRequestHeader", function(orig) {
+			return function(header, value) {
+				if (header === '__setXHR_') {
+					var val = value(this);
+					// fix for angular < 1.2.0
+					if (val instanceof Function) {
+						val(this);
 					}
-				})(xhr.setRequestHeader);
-				return xhr;
+				} else {
+					orig.apply(this, arguments);
+				}
 			}
-		})(window.XMLHttpRequest);
+		});
 	} else {
-		window.XMLHttpRequest = (function(origXHR) {
-			return function() {
-				var xhr = new origXHR();
-				var origSend = xhr.send;
-				xhr.__requestHeaders = [];
-				xhr.open = (function(orig) {
-					if (!xhr.upload) xhr.upload = {};
-					xhr.__listeners = [];
-					xhr.upload.addEventListener = function(t, fn, b) {
-						xhr.__listeners[t] = fn;
-					};
-					return function(m, url, b) {
-						orig.apply(xhr, [m, url, b]);
-						xhr.__url = url;
-					}
-				})(xhr.open);
-				xhr.getResponseHeader = (function(orig) {
-					return function(h) {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.getResponseHeader(h) : orig.apply(xhr, [h]);
-					}
-				})(xhr.getResponseHeader);
-				xhr.getAllResponseHeaders = (function(orig) {
-					return function() {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.getAllResponseHeaders() : orig.apply(xhr);
-					}
-				})(xhr.getAllResponseHeaders);
-				xhr.abort = (function(orig) {
-					return function() {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.abort() : (orig == null ? null : orig.apply(xhr));
-					}
-				})(xhr.abort);
-				xhr.setRequestHeader = (function(orig) {
-					return function(header, value) {
-						if (header === '__setXHR_') {
-							var val = value(xhr);
-							// fix for angular < 1.2.0
-							if (val instanceof Function) {
-								val(xhr);
-							}
-						} else {
-							orig.apply(xhr, arguments);
-						}
-					}
-				})(xhr.setRequestHeader);
-
-				xhr.send = function() {
-					if (arguments[0] && arguments[0].__isShim) {
-						var formData = arguments[0];
-						var config = {
-							url: xhr.__url,
-							complete: function(err, fileApiXHR) {
-								if (!err && xhr.__listeners['load']) 
-									xhr.__listeners['load']({type: 'load', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
-								if (!err && xhr.__listeners['loadend']) 
-									xhr.__listeners['loadend']({type: 'loadend', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
-								if (err === 'abort' && xhr.__listeners['abort']) 
-									xhr.__listeners['abort']({type: 'abort', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
-								if (fileApiXHR.status !== undefined) Object.defineProperty(xhr, 'status', {get: function() {return fileApiXHR.status}});
-								if (fileApiXHR.statusText !== undefined) Object.defineProperty(xhr, 'statusText', {get: function() {return fileApiXHR.statusText}});
-								Object.defineProperty(xhr, 'readyState', {get: function() {return 4}});
-								if (fileApiXHR.response !== undefined) Object.defineProperty(xhr, 'response', {get: function() {return fileApiXHR.response}});
-								Object.defineProperty(xhr, 'responseText', {get: function() {return fileApiXHR.responseText}});
-								xhr.__fileApiXHR = fileApiXHR;
-								xhr.onreadystatechange();
-							},
-							progress: function(e) {
-								e.target = xhr;
-								xhr.__listeners['progress'] && xhr.__listeners['progress'](e);
-								xhr.__total = e.total;
-								xhr.__loaded = e.loaded;
-							},
-							headers: xhr.__requestHeaders
-						}
-						config.data = {};
-						config.files = {}
-						for (var i = 0; i < formData.data.length; i++) {
-							var item = formData.data[i];
-							if (item.val != null && item.val.name != null && item.val.size != null && item.val.type != null) {
-								config.files[item.key] = item.val;
-							} else {
-								config.data[item.key] = item.val;
-							}
-						}
-
-						setTimeout(function() {
-							if (!hasFlash()) {
-								throw 'Adode Flash Player need to be installed. To check ahead use "FileAPI.hasFlash"';
-							}
-							xhr.__fileApiXHR = FileAPI.upload(config);
-						}, 1);
-					} else {
-						origSend.apply(xhr, arguments);
+		function initializeUploadListener(xhr) {
+			if (!xhr.__listeners) {
+				if (!xhr.upload) xhr.upload = {};
+				xhr.__listeners = [];
+				var origAddEventListener = xhr.upload.addEventListener;
+				xhr.upload.addEventListener = function(t, fn, b) {
+					xhr.__listeners[t] = fn;
+					origAddEventListener && origAddEventListener.apply(this, arguments);
+				};
+			}
+		}
+		
+		patchXHR("open", function(orig) {
+			return function(m, url, b) {
+				initializeUploadListener(this);
+				this.__url = url;
+				try {
+					orig.apply(this, [m, url, b]);
+				} catch (e) {
+					if (e.message.indexOf('Access is denied') > -1) {
+						orig.apply(this, [m, '_fix_for_ie_crossdomain__', b]);
 					}
 				}
-				return xhr;
 			}
-		})(window.XMLHttpRequest);
-		window.XMLHttpRequest.__hasFlash = hasFlash();
+		});
+
+		patchXHR("getResponseHeader", function(orig) {
+			return function(h) {
+				return this.__fileApiXHR ? this.__fileApiXHR.getResponseHeader(h) : orig.apply(this, [h]);
+			};
+		});
+
+		patchXHR("getAllResponseHeaders", function(orig) {
+			return function() {
+				return this.__fileApiXHR ? this.__fileApiXHR.abort() : (orig == null ? null : orig.apply(this));
+			}
+		});
+
+		patchXHR("abort", function(orig) {
+			return function() {
+				return this.__fileApiXHR ? this.__fileApiXHR.abort() : (orig == null ? null : orig.apply(this));
+			}
+		});
+
+		patchXHR("setRequestHeader", function(orig) {
+			return function(header, value) {
+				if (header === '__setXHR_') {
+					initializeUploadListener(this);
+					var val = value(this);
+					// fix for angular < 1.2.0
+					if (val instanceof Function) {
+						val(this);
+					}
+				} else {
+					this.__requestHeaders = this.__requestHeaders || {};
+					this.__requestHeaders[header] = value;
+					orig.apply(this, arguments);
+				}
+			}
+		});
+
+		patchXHR("send", function(orig) {
+			return function() {
+				var xhr = this;
+				if (arguments[0] && arguments[0].__isShim) {
+					var formData = arguments[0];
+					var config = {
+						url: xhr.__url,
+						complete: function(err, fileApiXHR) {
+							if (!err && xhr.__listeners['load']) 
+								xhr.__listeners['load']({type: 'load', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (!err && xhr.__listeners['loadend']) 
+								xhr.__listeners['loadend']({type: 'loadend', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (err === 'abort' && xhr.__listeners['abort']) 
+								xhr.__listeners['abort']({type: 'abort', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (fileApiXHR.status !== undefined) Object.defineProperty(xhr, 'status', {get: function() {return fileApiXHR.status}});
+							if (fileApiXHR.statusText !== undefined) Object.defineProperty(xhr, 'statusText', {get: function() {return fileApiXHR.statusText}});
+							Object.defineProperty(xhr, 'readyState', {get: function() {return 4}});
+							if (fileApiXHR.response !== undefined) Object.defineProperty(xhr, 'response', {get: function() {return fileApiXHR.response}});
+							Object.defineProperty(xhr, 'responseText', {get: function() {return fileApiXHR.responseText}});
+							Object.defineProperty(xhr, 'response', {get: function() {return fileApiXHR.responseText}});
+							xhr.__fileApiXHR = fileApiXHR;
+							if (xhr.onreadystatechange) xhr.onreadystatechange();
+						},
+						fileprogress: function(e) {
+							e.target = xhr;
+							xhr.__listeners['progress'] && xhr.__listeners['progress'](e);
+							xhr.__total = e.total;
+							xhr.__loaded = e.loaded;
+						},
+						headers: xhr.__requestHeaders
+					}
+					config.data = {};
+					config.files = {}
+					for (var i = 0; i < formData.data.length; i++) {
+						var item = formData.data[i];
+						if (item.val != null && item.val.name != null && item.val.size != null && item.val.type != null) {
+							config.files[item.key] = item.val;
+						} else {
+							config.data[item.key] = item.val;
+						}
+					}
+
+					setTimeout(function() {
+						if (!hasFlash()) {
+							throw 'Adode Flash Player need to be installed. To check ahead use "FileAPI.hasFlash"';
+						}
+						xhr.__fileApiXHR = FileAPI.upload(config);
+					}, 1);
+				} else {
+					orig.apply(xhr, arguments);
+				}
+			}
+		});
 	}
 	window.XMLHttpRequest.__isShim = true;
 }
 
-if (!window.FormData) {
-	var wrapFileApi = function(elem) {
+if (!window.FormData || (window.FileAPI && FileAPI.forceLoad)) {
+	var addFlash = function(elem) {
 		if (!hasFlash()) {
 			throw 'Adode Flash Player need to be installed. To check ahead use "FileAPI.hasFlash"';
 		}
-		if (!elem.__isWrapped && (elem.getAttribute('ng-file-select') != null || elem.getAttribute('data-ng-file-select') != null)) {
-			var wrap = document.createElement('div');
-			wrap.innerHTML = '<div class="js-fileapi-wrapper" style="position:relative; overflow:hidden"></div>';
-			wrap = wrap.firstChild;
-			var parent = elem.parentNode;
-			parent.insertBefore(wrap, elem);
-			parent.removeChild(elem);
-			wrap.appendChild(elem);
-			elem.__isWrapped = true;
+		var el = angular.element(elem);
+		if (!el.hasClass('js-fileapi-wrapper') && (elem.getAttribute('ng-file-select') != null || elem.getAttribute('data-ng-file-select') != null)) {
+			if (FileAPI.wrapInsideDiv) {
+				var wrap = document.createElement('div');
+				wrap.innerHTML = '<div class="js-fileapi-wrapper" style="position:relative; overflow:hidden"></div>';
+				wrap = wrap.firstChild;
+				var parent = elem.parentNode;
+				parent.insertBefore(wrap, elem);
+				parent.removeChild(elem);
+				wrap.appendChild(elem);
+			} else {
+				el.addClass('js-fileapi-wrapper');
+			}
 		}
 	};
 	var changeFnWrapper = function(fn) {
 		return function(evt) {
 			var files = FileAPI.getFiles(evt);
+			//just a double check for #233
+			for (var i = 0; i < files.length; i++) {
+				if (files[i].size === undefined) files[i].size = 0;
+				if (files[i].name === undefined) files[i].name = 'file';
+				if (files[i].type === undefined) files[i].type = 'undefined';
+			}
 			if (!evt.target) {
 				evt.target = {};
 			}
 			evt.target.files = files;
-			evt.target.files.item = function(i) {
-				return evt.target.files[i] || null;
+			// if evt.target.files is not writable use helper field
+			if (evt.target.files != files) {
+				evt.__files_ = files;
 			}
-			fn(evt);
+			(evt.__files_ || evt.target.files).item = function(i) {
+				return (evt.__files_ || evt.target.files)[i] || null;
+			}
+			if (fn) fn.apply(this, [evt]);
 		};
 	};
 	var isFileChange = function(elem, e) {
@@ -176,7 +204,7 @@ if (!window.FormData) {
 		HTMLInputElement.prototype.addEventListener = (function(origAddEventListener) {
 			return function(e, fn, b, d) {
 				if (isFileChange(this, e)) {
-					wrapFileApi(this);
+					addFlash(this);
 					origAddEventListener.apply(this, [e, changeFnWrapper(fn), b, d]);
 				} else {
 					origAddEventListener.apply(this, [e, fn, b, d]);
@@ -188,8 +216,13 @@ if (!window.FormData) {
 		HTMLInputElement.prototype.attachEvent = (function(origAttachEvent) {
 			return function(e, fn) {
 				if (isFileChange(this, e)) {
-					wrapFileApi(this);
-					origAttachEvent.apply(this, [e, changeFnWrapper(fn)]);
+					addFlash(this);
+					if (window.jQuery) {
+						// fix for #281 jQuery on IE8
+						angular.element(this).bind("change", changeFnWrapper(null));
+					} else {
+						origAttachEvent.apply(this, [e, changeFnWrapper(fn)]);
+					}
 				} else {
 					origAttachEvent.apply(this, [e, fn]);
 				}
@@ -216,6 +249,10 @@ if (!window.FormData) {
 		if (!window.FileAPI) {
 			window.FileAPI = {};
 		}
+		if (FileAPI.forceLoad) {
+			FileAPI.html5 = false;
+		}
+		
 		if (!FileAPI.upload) {
 			var jsUrl, basePath, script = document.createElement('script'), allScripts = document.getElementsByTagName('script'), i, index, src;
 			if (window.FileAPI.jsUrl) {
