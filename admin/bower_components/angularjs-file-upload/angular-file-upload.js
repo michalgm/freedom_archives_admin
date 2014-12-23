639f3d507f60f6a8585dc335452449dbabde0ae5
Greg Michalec <greg@primate.net>
2014-12-22 23:11:33 -0800

    updated js deps

--- admin/bower_components/angularjs-file-upload/angular-file-upload.js
+++ admin/bower_components/angularjs-file-upload/angular-file-upload.js
@@ -1,7 +1,7 @@
 /**!
  * AngularJS file upload/drop directive with http post and progress
  * @author  Danial  <danial.farid@gmail.com>
- * @version 1.6.4
+ * @version 1.6.12
  */
 (function() {
 
@@ -145,16 +145,35 @@ angularFileUpload.directive('ngFileSelect', [ '$parse', '$timeout', function($pa
 		var fn = $parse(attr['ngFileSelect']);
 		if (elem[0].tagName.toLowerCase() !== 'input' || (elem.attr('type') && elem.attr('type').toLowerCase()) !== 'file') {
 			var fileElem = angular.element('<input type="file">')
-			for (var i = 0; i < elem[0].attributes.length; i++) {
-				fileElem.attr(elem[0].attributes[i].name, elem[0].attributes[i].value);
+			var attrs = elem[0].attributes;
+			for (var i = 0; i < attrs.length; i++) {
+				if (attrs[i].name.toLowerCase() !== 'type') {
+					fileElem.attr(attrs[i].name, attrs[i].value);
+				}
 			}
-			if (elem.attr("data-multiple")) fileElem.attr("multiple", "true");
-			fileElem.css("top", 0).css("bottom", 0).css("left", 0).css("right", 0).css("width", "100%").
-					css("opacity", 0).css("position", "absolute").css('filter', 'alpha(opacity=0)');
+			if (attr["multiple"]) fileElem.attr("multiple", "true");
+			fileElem.css("width", "1px").css("height", "1px").css("opacity", 0).css("position", "absolute").css('filter', 'alpha(opacity=0)')
+					.css("padding", 0).css("margin", 0).css("overflow", "hidden");
+			fileElem.attr('__wrapper_for_parent_', true);
+
+//			fileElem.css("top", 0).css("bottom", 0).css("left", 0).css("right", 0).css("width", "100%").
+//					css("opacity", 0).css("position", "absolute").css('filter', 'alpha(opacity=0)').css("padding", 0).css("margin", 0);
 			elem.append(fileElem);
-			if (elem.css("position") === '' || elem.css("position") === 'static') {
-				elem.css("position", "relative");
-			}
+			elem[0].__file_click_fn_delegate_  = function() {
+				fileElem[0].click();
+			}; 
+			elem.bind('click', elem[0].__file_click_fn_delegate_);
+			elem.css("overflow", "hidden");
+//			if (fileElem.parent()[0] != elem[0]) {
+//				//fix #298 button element
+//				elem.wrap('<span>');
+//				elem.css("z-index", "-1000")
+//				elem.parent().append(fileElem);
+//				elem = elem.parent();
+//			}
+//			if (elem.css("position") === '' || elem.css("position") === 'static') {
+//				elem.css("position", "relative");
+//			}
 			elem = fileElem;
 		}
 		elem.bind('change', function(evt) {
@@ -205,13 +224,11 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 		if ('draggable' in document.createElement('span')) {
 			var leaveTimeout = null;
 			elem[0].addEventListener("dragover", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 				$timeout.cancel(leaveTimeout);
 				if (!elem[0].__drag_over_class_) {
-					if (attr['ngFileDragOverClass'].search(/\) *$/) > -1) {
-						dragOverClassFn = $parse(attr['ngFileDragOverClass']);
-						var dragOverClass = dragOverClassFn(scope, {
+					if (attr['ngFileDragOverClass'] && attr['ngFileDragOverClass'].search(/\) *$/) > -1) {
+						var dragOverClass = $parse(attr['ngFileDragOverClass'])(scope, {
 							$event : evt
 						});					
 						elem[0].__drag_over_class_ = dragOverClass; 
@@ -222,7 +239,6 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 				elem.addClass(elem[0].__drag_over_class_);
 			}, false);
 			elem[0].addEventListener("dragenter", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 			}, false);
 			elem[0].addEventListener("dragleave", function(evt) {
@@ -233,7 +249,6 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 			}, false);
 			var fn = $parse(attr['ngFileDrop']);
 			elem[0].addEventListener("drop", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 				elem.removeClass(elem[0].__drag_over_class_);
 				elem[0].__drag_over_class_ = null;
@@ -259,7 +274,7 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 							//fix for chrome bug https://code.google.com/p/chromium/issues/detail?id=149735
 							if (isASCII(entry.name)) {
 								traverseFileTree(files, entry);
-							} else {
+							} else if (!items[i].webkitGetAsEntry().isDirectory) {
 								files.push(items[i].getAsFile());
 							}
 						}
@@ -284,14 +299,14 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 			}
 			
 			var processing = 0;
-			function traverseFileTree(files, entry) {
+			function traverseFileTree(files, entry, path) {
 				if (entry != null) {
 					if (entry.isDirectory) {
 						var dirReader = entry.createReader();
 						processing++;
 						dirReader.readEntries(function(entries) {
 							for (var i = 0; i < entries.length; i++) {
-								traverseFileTree(files, entries[i]);
+								traverseFileTree(files, entries[i], (path ? path : "") + entry.name + "/");
 							}
 							processing--;
 						});
@@ -299,6 +314,7 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 						processing++;
 						entry.file(function(file) {
 							processing--;
+							file._relativePath = (path ? path : "") + file.name;
 							files.push(file);
 						});
 					}
